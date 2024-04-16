e979f77868f31daf852b5b796fe1d8ee5203b2a6
Thibault <thibault.barral@epsi.fr>
2024-04-16 13:43:25 +0200

    add_board

--- app/controllers/online-game.controller.js
+++ app/controllers/online-game.controller.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SocketContext } from '../contexts/socket.context';
+import Board from "../components/board/board.component";
 
 
 export default function OnlineGameController() {
@@ -54,18 +55,7 @@ export default function OnlineGameController() {
 
             {inGame && (
                 <>
-                    <Text style={styles.paragraph}>
-                        Game found !
-                    </Text>
-                    <Text style={styles.paragraph}>
-                        Player - {socket.id} -
-                    </Text>
-                    <Text style={styles.paragraph}>
-                        - vs -
-                    </Text>
-                    <Text style={styles.paragraph}>
-                        Player - {idOpponent} -
-                    </Text>
+                    <Board />
                 </>
             )}
         </View>
